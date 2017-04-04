5c929a92de1bd59bd3f67604857f4f339131cf89
mike.barden.pro@gmail.com <ncoOIOfUryoPjfRmPAbR>
2017-04-05 02:09:53 +0300

    moved

--- src/com/autovitalsinc/videotrimmer/VideoTrimmer.java
+++ src/com/autovitalsinc/videotrimmer/VideoTrimmer.java
@@ -12,12 +12,18 @@ import org.json.JSONObject;
 import org.json.JSONArray;
 import org.json.JSONException;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.util.Log;
 
 import java.util.Date;
 
+import life.knowledge4.videotrimmer.K4LVideoTrimmer;
+
 public class VideoTrimmer extends CordovaPlugin {
 	private static final String TAG = "VideoTrimmer";
+	
+	private CallbackContext cbContext;
 
 	public void initialize(CordovaInterface cordova, CordovaWebView webView) {
 		super.initialize(cordova, webView);
@@ -26,7 +32,21 @@ public class VideoTrimmer extends CordovaPlugin {
 	}
 
 	public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
+		
+		this.cbContext=callbackContext;
+		
+		String path = args.getString(0);
+		
 		if(action.equals("trim")) {
+			
+			K4LVideoTrimmer videoTrimmer = ((K4LVideoTrimmer) cordova.getActivity().getWindow().getDecorView().findViewById(getResId("timeLine", "id")));
+            if (videoTrimmer != null) {
+                videoTrimmer.setVideoURI(Uri.parse(path));
+            }
+            
+            Intent intent = new Intent("life.knowledge4.videotrimmer.K4LVideoTrimmer");
+
+            this.cordova.startActivityForResult((CordovaPlugin) this, intent, 0);
 
 			
 		}
@@ -35,7 +55,13 @@ public class VideoTrimmer extends CordovaPlugin {
 
 	public boolean getResult(){
 		final PluginResult result = new PluginResult(PluginResult.Status.OK, (new Date()).toString());
-		callbackContext.sendPluginResult(result);
+		this.cbContext.sendPluginResult(result);
+		
+		return true;
 	}
+	
+    private int getResId(String name, String type) {
+        return cordova.getActivity().getResources().getIdentifier(name, type, cordova.getActivity().getPackageName());
+    }
 
 }
